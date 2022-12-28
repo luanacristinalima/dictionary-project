f3bad5a3db68581236c9457c1aabccc1a2a34d5f
Luana Lima <luana.arqeurb@gmail.com>
2022-12-28 11:00:54 +0000

    Inicial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary Project</h1>
+      </header>
+    </div>
+  );
+}
