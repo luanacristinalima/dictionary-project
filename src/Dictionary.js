99a3e36b158818c9d699fecf2c33668978e88002
Luana Lima <luana.arqeurb@gmail.com>
2022-12-28 12:25:26 +0000

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -7,9 +8,15 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
 
   return (
