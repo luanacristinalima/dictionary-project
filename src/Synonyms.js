eae58607816792c86cb09a1a0707311e4260bebf
Luana Lima <luana.arqeurb@gmail.com>
2022-12-30 12:06:45 +0000

    Change styling of synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,7 +3,11 @@ export default function Synonyms(props) {
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
+          return (
+            <li key={index} className="text-capitalize">
+              {synonym}
+            </li>
+          );
         })}
       </ul>
     );
